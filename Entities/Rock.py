18c381aefbb56895938027db4352210a3efc5c68
Your Name <you@example.com>
2017-09-14 01:03:54 +0200

    update

---
+++ Entities/Rock.py
@@ -0,0 +1,117 @@
+from PyGnin import *
+import pygame
+import random
+import math
+
+
+class Rock(Game.Sprite):
+
+    def __init__(self):
+        super().__init__()
+
+        self.debug_rocks = False
+        self._rock_tileset = Render.TileSet("assets/rocks_rotated.png", (256, 256))
+        self._rock_tileset.set_scale(0.6)
+        self._tile_range = (0, 7)
+        self._range_between_rocks = (100, 3000)
+        self._max_nb_rocks = 60
+        self._rocks = [{"tile": [0, 0], "pos": [0, 0]} for x in range(self._max_nb_rocks)]
+
+    @staticmethod
+    def calc_dist(p1, p2):
+
+        return math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+
+    def generate(self):
+
+        used_pos = []
+        for rock in self._rocks:
+
+            keep_going = True
+
+            tile_x = random.randint(self._tile_range[0], self._tile_range[1])
+            tile_y = random.randint(self._tile_range[0], self._tile_range[1])
+
+            while keep_going:
+
+                # NEW ALGO
+                current_pos = list()
+
+                if not current_pos:
+
+                    current_pos = (random.randint(0, 3200), random.randint(0, 3200))  # voir pour le self.get_size
+                    # LOG ##############################
+                    # print("current" + repr(current_pos))
+                    # ##################################
+
+                    if used_pos:
+
+                        for pos in used_pos:
+                            dist = int(self.calc_dist(current_pos, pos))
+
+                            # LOG ###########################################################################
+                            # print("current:" + repr(current_pos) + " used:" + repr(pos) + " dist:" + repr(int(dist)))
+                            # ###############################################################################
+
+                            if dist not in range(self._range_between_rocks[0], self._range_between_rocks[1]):
+                                # LOG #########################################################
+                                # print("bad_range = " + repr(dist)
+                                #       + " !(" + repr(self._range_between_rocks[0])
+                                #       + " <> " + repr(self._range_between_rocks[1]) + ")")
+                                # #############################################################
+                                current_pos = list()
+                                break
+
+                # NEXT
+                if current_pos:
+                    used_pos.append(current_pos)
+
+                    # LOG #########
+                    # print("LIST POINTS ====> " + repr(used_pos))
+                    # print("LIST LEN    ====> " + repr(len(used_pos)))
+                    # #############
+
+                    keep_going = False
+
+            rock["tile"][0] = tile_x
+            rock["tile"][1] = tile_y
+            rock["pos"][0] = current_pos[0]
+            rock["pos"][1] = current_pos[1]
+
+        # LOG ############
+        print(self._rocks)
+        # print(used_pos)
+        # ################
+
+    def draw(self, surface, camera=None):
+
+        tile_w, tile_h = self._rock_tileset.get_tile_size()
+
+        for rock in self._rocks:
+
+            tile_x = rock["tile"][0]
+            tile_y = rock["tile"][1]
+            pos_x = rock["pos"][0]
+            pos_y = rock["pos"][1]
+
+            if camera:
+                pos_x -= camera.get_position()[0]
+                pos_y -= camera.get_position()[1]
+
+            pos_x -= tile_w / 2
+            pos_y -= tile_h / 2
+
+            # DEBUG ##############
+            if self.debug_rocks:
+                a = (pos_x, pos_y)
+                c = (pos_x + tile_w, pos_y + tile_h)
+                pygame.draw.circle(surface, (255, 0, 0),
+                                   (int((a[0] + c[0]) / 2), int((a[1] + c[1]) / 2)),
+                                   self._range_between_rocks[0], 2)
+                pygame.draw.rect(surface, (0, 0, 0), (pos_x, pos_y, tile_w, tile_h), 2)
+            # ####################
+
+            self._rock_tileset.draw_tile(tile_x, tile_y, pos_x, pos_y, screen=surface)
+
+        if IO.Keyboard.is_down(K_r):
+            self.debug_rocks = not self.debug_rocks
