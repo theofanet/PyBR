f3209b1c32c97f13fed02a74737114445df4e023
guillaume laborde <guillaume@16pixels.fr>
2018-09-17 19:50:36 +0200

    update

--- Scenes/Test.py
+++ Scenes/Test.py
@@ -1,77 +1,147 @@
 from PyGnin import *
+from random import randint
 import pygame
 
-MLTP = 3
-DIFFICULTY = 4
+LOCKERS_NB = 30
+LOCKERS_LENGHT = 10
+LOCKERS_WIDTH = 40
 
-LOCKER_LENGHT = 10
-LOCKER_WIDTH = 40
+LOCKER_UP = 0
+LOCKER_MID = 1
+LOCKER_DOWN = 2
 
+SCREEN_W = 600
+SCREEN_L = 800
+SCREEN_CENTER_X = SCREEN_L / 2
+SCREEN_CENTER_Y = SCREEN_W / 2
+
+
+# grid entity.
+class Grid:
+
+    def __init__(self):
+        self.l = LOCKERS_NB * LOCKERS_LENGHT * 2
+        self.w = LOCKERS_WIDTH * 2
+
+        self.y = SCREEN_CENTER_Y - (self.w / 2)
+        self.x = SCREEN_CENTER_X - (self.l / 2)
+
+        self.start_pos = SCREEN_CENTER_X + (self.l / 2)
+        self.rect = None
+
+        self.lockers_list = list()
+
+    # init a list of lockers, depend of LOCKERS_NB.
+    def init_lockers(self, nb_lockers: int):
+        locker_modifier = LOCKERS_LENGHT * 1.5
+        selector_modifier = LOCKERS_LENGHT * 1.7
+
+        for i in range(nb_lockers):
+            locker = Locker()
+            locker_pos_y = SCREEN_CENTER_Y - (locker.w / 2)
+            locker_pos_x = self.start_pos - locker_modifier
+            # random assign position.
+            locker.position = randint(0, 2)
+            y_modifier = 0
+            if locker.position == 0:
+                y_modifier -= 30
+            elif locker.position == 2:
+                y_modifier += 30
+
+            locker.rect = pygame.Rect(locker_pos_x, locker_pos_y + y_modifier, locker.l, locker.w)
+
+            selector_pos_y = SCREEN_CENTER_Y - (locker.w * 1.5)
+            selector_pos_x = self.start_pos - selector_modifier
+            locker.selector.rect = pygame.Rect(selector_pos_x, selector_pos_y, locker.selector.l, locker.selector.w)
+
+            self.lockers_list.append(locker)
+
+            if i == 0:
+                locker.is_selected = True
+
+            locker_modifier += LOCKERS_LENGHT * 2
+            selector_modifier += LOCKERS_LENGHT * 2
+
+
+# locker entity.
+class Locker:
+
+    def __init__(self):
+        self.l = LOCKERS_LENGHT
+        self.w = LOCKERS_WIDTH
+        # self.modifier = self.l * 1.5
+        self.selector = Selector()
+        self.is_selected = False
+        self.rect = None
+        self.position = None
+
+
+# selector entity.
+class Selector:
+
+    def __init__(self):
+        self.l = LOCKERS_LENGHT * 1.5
+        self.w = LOCKERS_WIDTH * 3
+        self.modifier = LOCKERS_LENGHT * 1.7
+        self.rect = None
+
+
+# full scene.
 class TestScene(Game.Scene):
 
     def __init__(self):
         super().__init__()
-
-        # screen size.
-        self._screenW = 600
-        self._screenL = 800
-
         # font.
         self._font = Render.Font("assets/Permanent_Marker/PermanentMarker-Regular.ttf")
-
-        # nb lockers for a grid.
-        self._nbLockers = DIFFICULTY * MLTP
-        self._lockersList = list()
-
-        # set grid size.
-        self._gridLenght = self._nbLockers * LOCKER_LENGHT * 2
-        self._gridWidth = LOCKER_WIDTH * 2
-
-        # set grid position.
-        self._xcenter = self._screenL / 2
-        self._ycenter = self._screenW / 2
-        self._gridY = self._ycenter - (self._gridWidth / 2)
-        self._gridX = self._xcenter - (self._gridLenght / 2)
-
-        # set selector size.
-        self._selectLenght = LOCKER_LENGHT * 1.5
-        self._selectWidth = LOCKER_WIDTH * 3
+        # grid.
+        self._grid = Grid()
 
     def _load_resources(self):
-        self._grid = pygame.Rect(self._gridX, self._gridY, self._gridLenght, self._gridWidth)
-        self._selector = pygame.Rect((self._xcenter + (self._gridLenght / 2) - (LOCKER_LENGHT * 1.7)), self._ycenter - (self._selectWidth / 2), self._selectLenght, self._selectWidth)
+        # grid Rect.
+        self._grid.rect = pygame.Rect(self._grid.x, self._grid.y, self._grid.l, self._grid.w)
+        self._grid.init_lockers(LOCKERS_NB)
 
     def update(self):
-        # TODO: refact tout ce bordel avec la merde du dessus.
-        start = self._xcenter + (self._gridLenght / 2)
-        selectModifier = LOCKER_LENGHT * 1.7
-        lockerModifier = LOCKER_LENGHT * 1.5
-        ly = self._ycenter - (LOCKER_WIDTH / 2)
+        # UP !
+        if IO.Keyboard.is_down(K_UP):
+            i = 0
+            for l in self._grid.lockers_list:
+                print(i)
+                if l.is_selected:
+                    if l.position > LOCKER_UP:
+                        l.rect.y -= 30
+                        l.position -= 1
+                    l.is_selected = False
 
-        # TODO: switch selected locker state and move selector next.
-        if IO.Keyboard.is_down(K_LEFT):
-            if self._selector.x < (start - selectModifier) - (selectModifier * self._nbLockers):
-                self._selector.x = start - selectModifier
-            else:
-                self._selector.x -= LOCKER_LENGHT * 2
-        elif IO.Keyboard.is_down(K_RIGHT):
-            if self._selector.x < start - selectModifier:
-                self._selector.x += LOCKER_LENGHT * 2
+                    if i < len(self._grid.lockers_list) - 1:
+                        self._grid.lockers_list[i+1].is_selected = True
+                    else:
+                        self._grid.lockers_list[0].is_selected = True
+                    break
+                i += 1
+        # DOWN !
+        elif IO.Keyboard.is_down(K_DOWN):
+            i = 0
+            for l in self._grid.lockers_list:
+                print(i)
+                if l.is_selected:
+                    if l.position < LOCKER_DOWN:
+                        l.rect.y += 30
+                        l.position += 1
+                    l.is_selected = False
 
-        # lockers positions.
-        # TODO: update lockers up and down. [is_selected]
-        # TODO: switch les rec dans le load ou init.
-        self._lockersList = list() # reset.
-        for i in range(self._nbLockers):
-            lx = (self._xcenter + (self._gridLenght / 2) - lockerModifier)
-            locker = pygame.Rect(lx, ly, LOCKER_LENGHT, LOCKER_WIDTH)
-            self._lockersList.append(locker)
-            lockerModifier += LOCKER_LENGHT * 2
+                    if i < len(self._grid.lockers_list) - 1:
+                        self._grid.lockers_list[i+1].is_selected = True
+                    else:
+                        self._grid.lockers_list[0].is_selected = True
+                    break
+                i += 1
 
     def draw(self):
         self._font.draw_text("test", (380, 20), (255, 0, 0))
         pygame.draw.rect(App.get_display(), (255, 0, 0), self._grid, 1)
-        pygame.draw.rect(App.get_display(), (0, 0, 255), self._selector, 1)
 
-        for x in self._lockersList:
-            pygame.draw.rect(App.get_display(), (0, 255, 255), x)
+        for x in self._grid.lockers_list:
+            pygame.draw.rect(App.get_display(), (0, 255, 255), x.rect)
+            if x.is_selected:
+                pygame.draw.rect(App.get_display(), (0, 0, 255), x.selector.rect, 1)
